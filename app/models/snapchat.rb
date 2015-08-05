35aeb01dd29090265980f31615469c48b10030b2
Whasame <coolmaxalt@gmail.com>
2015-08-05 17:41:53 +0000

    commented snapchat

--- app/models/snapchat.rb
+++ app/models/snapchat.rb
@@ -1,30 +1,31 @@
 class Snapchat
-	attr_reader :to_user, :caption, :timer, :time, :format, :link
-	attr_accessor :read
+# 	attr_reader :to_user, :caption, :timer, :time, :format, :link
+# 	attr_accessor :read
 
-	SNAPS = []
+# 	SNAPS = []
 
-	def initialize(to_user, caption, timer, time, format, link)
-# 		@user = user
-		@to_user = to_user
-		@caption = caption
-		@timer = timer
-		@time = time
-		@link = link
-		@read = false
+# 	def initialize(to_user, caption, timer, time, format, link)
+# # 		@user = user
+# 		@to_user = to_user
+# 		@caption = caption
+# 		@timer = timer
+# 		@time = time
+# 		@link = link
+# 		@read = false
 
-		SNAPS << self
+# 		SNAPS << self
 
-		if(format == "png" || format == "jpg" ||format == "jpeg")
-			format = "pic"
-		elsif (format == "mov" || format == "avi")
-			format = "video"
-		end
-	end
+# 		if(format == "png" || format == "jpg" ||format == "jpeg")
+# 			format = "pic"
+# 		elsif (format == "mov" || format == "avi")
+# 			format = "video"
+# 		end
+# 	end
 
-	def self.all
-		SNAPS
-	end
+# 	def self.all
+# 		SNAPS
+	
+# 	end
 
 
 end
