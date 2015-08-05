36c7915e3a6d776b0e4fddade332af12ad9e8c8d
Whasame <coolmaxalt@gmail.com>
2015-08-05 01:47:30 +0000

    didnt rly add much

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < Sinatra::Base
   get '/' do
 		time = Time.new
 		# 		this is just test code to test multiple tweets, we can delete it later
-		@snap = Snapchat.new("params[:to]", "test caption", 5, "Sent at #{time.hour}:#{time.min}", "png", "http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg")
-		@snap = Snapchat.new("params[:to]", "test caption", 3, "Sent at #{time.hour}:#{time.min}", "png", "http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg")
+		snap1 = Snapchat.new("params[:to]", "test caption", 5, "Sent at #{time.hour}:#{time.min}", "png", "http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg")
+		snap2 = Snapchat.new("params[:to]", "test caption", 3, "Sent at #{time.hour}:#{time.min}", "png", "http://www.lauramcalister.com/wp-content/uploads/2015/07/534806-cat.jpg")
     @snaps = Snapchat.all
 		erb :index
   end
@@ -35,9 +35,9 @@ class ApplicationController < Sinatra::Base
 			@error = "TIME LIMIT"
 			erb :noPic
 		else
-			@snap = Snapchat.new(params[:to], params[:cap], params[:time], "#{time.hour}:#{time.min}", "png", params[:url])
-		@snaps = Snapchat.all
-		erb :index
+			snap = Snapchat.new(params[:to], params[:cap], params[:time], "#{time.hour}:#{time.min}", "png", params[:url])
+			@snaps = Snapchat.all
+			erb :index
 		end
 		
 	end
