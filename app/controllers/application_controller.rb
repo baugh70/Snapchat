0c2c474f3cdab5aabc2b6b8f9e67b7d213c32cf2
Whasame <coolmaxalt@gmail.com>
2015-08-07 15:45:38 +0000

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,11 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+		enable :sessions
+		set :session_secret, 'admin123'
   end
   
   get '/' do
-# 		time = Time.new
-# 		# 		this is just test code to test multiple tweets, we can delete it later
-# 		snap1 = Snap.new("params[:to]", "test caption", 5, "Sent at #{time.hour}:#{time.min}", "png", "http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg")
-# 		@snap2 = Snap.new("params[:to]", "test caption", 3, "Sent at #{time.hour}:#{time.min}", "png", "http://www.lauramcalister.com/wp-content/uploads/2015/07/534806-cat.jpg")
-#     @snap1.save
 		erb :index
   end
 	
@@ -44,15 +41,16 @@ class ApplicationController < Sinatra::Base
 	end
 	
 	get '/new' do
+	if session[:user_id] != nil
 		erb :snapchat
+	else
+			redirect '/'
+		end
 	end
 	
 	post '/new' do
-		
-# 		@user = User.find_by(:to => params[:to])
-		
+				if session[:user_id] != nil
     time  = Time.new
-		
 		if (params[:url] == "") || (params[:url][0..3].downcase != "http")
 			@error = "URL"
 			@link = "/new"
@@ -73,12 +71,20 @@ class ApplicationController < Sinatra::Base
       @snaps = Snap.all
       redirect '/snaps'
 		end
+				else
+			redirect '/'
+		end
 		
 	end
 	
 	get '/snaps' do
+		if session[:user_id] != nil
+		@username = User.find_by(session[:user_id]).username.capitalize
 		@snaps = Snap.all
-		erb :snaps
+			erb :snaps
+		else
+			redirect '/'
+		end
 	end
 
 	get '/register' do
@@ -86,12 +92,10 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get '/login' do
-		puts "get login"
 		erb :login
 	end
 
 	post '/login' do
-		puts "login"
 		username = params[:username]
 		password = params[:password]
 		if (username == "")
@@ -105,39 +109,43 @@ class ApplicationController < Sinatra::Base
 		else
 			if (username.include? "@")
 			user_by_email = User.find_by(:email => username)
-			@id = user_by_email.user_id
 			puts "else in post login"
 			if (password == user_by_email.password)
-				redirect '/snaps'
+				if user_by_email
+					@username = user_by_email.username
+					session[:user_id] = user_by_email.id
+					redirect '/snaps'
+				else
+					redirect '/register'
+				end
 			else
 				@error = "PASSWORD"
 				@link = "/login"
 				erb :noPic
 			end
 			else
-				user_by_name = User.find_by(:username => username)
-			@id = user_by_email.user_id
-			puts "else in post login"
+			user_by_name = User.find_by(:username => username)
 			if (password == user_by_name.password)
-				redirect '/snaps'
+				if user_by_name
+					@username = user_by_name.username
+					puts @username
+					session[:user_id] = user_by_name.id
+					redirect '/snaps'
+				else
+					redirect '/register'
+				end
 			else
 				@error = "PASSWORD"
 				@link = "/login"
 				erb :noPic
 			end
 			end
-			# user_by_name = User.find_by(:username => username)
-
-			# puts "else in post login"
-			# if (password == user_by_name.password) || (password == user_by_email.password)
-			# 	redirect '/snaps'
-			# else
-			# 	@error = "PASSWORD"
-			# 	@link = "/login"
-			# 	erb :noPic
-			# end
-
 		end
+	end
+	
+	get '/logout' do
+		session[:user_id] = nil
+		redirect '/'
 	end
 	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806200110) do
+ActiveRecord::Schema.define(version: 20150807131134) do
 
   create_table "snaps", force: :cascade do |t|
     t.string  "to"
@@ -25,11 +25,10 @@ ActiveRecord::Schema.define(version: 20150806200110) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "password"
-    t.string  "email"
-    t.string  "friends"
-    t.integer "user_id"
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.string "friends"
   end
 
 end
